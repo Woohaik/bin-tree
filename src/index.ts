0d21cdc9c2b6f4c7b5b3f58240b746f485f5357b
Wilfredo Hernández <74992119+Woohaik@users.noreply.github.com>
2020-11-26 19:30:27 +0100

    Importing work

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
-import { BinaryTree } from "./BinaryTree";
-
 export { BinaryTree } from "./BinaryTree";
+
 export { Node } from "./Node";
 
 
-
-
-
-const defaultFunction = (value: Number = null, content: any = null) => {
-    return new BinaryTree(value, content);
-}
-
-
-module.exports = defaultFunction;
